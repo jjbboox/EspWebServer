717334ccc7b6e3228a2cfc8f32ceabb94b8e862f
jjbboox <jjbboox@163.com>
2019-02-08 11:21:29 +0800

    将baiduTranslate.h加入.gitignore

--- include/baiduTranslate.h
+++ include/baiduTranslate.h
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 
 const String host = "http://api.fanyi.baidu.com/api/trans/vip/translate";
-const String appid = "xxxxxxxxxxxxxxxxxxxxx";
-const String apiKey = "xxxxxxxxxxxxxxxxxxxxxx";
+const String appid = "20190120000257962";
+const String apiKey = "a7YX_Rr5O_aJxyD1GWWG";
 
 #endif  // _BAIDU_TRANSLATE_H_
