b74039217dd4d9e19410e47b6ffd879f6734f3f8
jjbboox <jjbboox@gmail.com>
2019-03-13 23:54:03 +0800

    delete comment

--- include/myWebServerHandle.h
+++ include/myWebServerHandle.h
@@ -6,11 +6,8 @@ void myWebServerOnSetup();
 void handleInit(AsyncWebServerRequest *request);
 void handleSwitchLed(AsyncWebServerRequest *request);
 void handleTranslate(AsyncWebServerRequest *request);
-// void handleTranslate(AsyncWebServerRequest *request, uint8_t* data);
 void handleGetTemperatuer(AsyncWebServerRequest *request);
-// void handleSetRGBColor(AsyncWebServerRequest *request);
 void handleSetRGBColor(AsyncWebServerRequest *request);
-// void handleSetRGBColor(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total);
 void handleDHTLogs(AsyncWebServerRequest *request);
 void handleGetPir(AsyncWebServerRequest *request);
 
